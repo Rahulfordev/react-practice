802879e98cd929c38afb4f2c2276c0ad231098b1
rahulfordev <rahulforsms@gmail.com>
2023-11-27 19:13:24 +0600

    form handling using formki package

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import "./App.css";
 // import Form from "./components/Form/Form";
 // import RouteComponent from "./pages/Route";
 // import Parent from "./components/Data-passing/State-lefting/child-to-parent-1/Parent";
-import Home from "./components/Data-passing/State-lefting/child-to-parent-2/Home";
+// import Home from "./components/Data-passing/State-lefting/child-to-parent-2/Home";
+import Signup from "./components/Signup/Signup";
 function App() {
   return (
     <>
@@ -10,7 +11,8 @@ function App() {
         {/* <RouteComponent /> */}
         {/* <Form /> */}
         {/* <Parent /> */}
-        <Home />
+        {/* <Home /> */}
+        <Signup />
       </div>
     </>
   );
---
+++ src/components/Signup/Signup.jsx
@@ -0,0 +1,55 @@
+import { useFormik } from "formik";
+
+const Signup = () => {
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+    onSubmit: (values, { resetForm }) => {
+      console.log(values);
+      resetForm({ values: "" });
+    },
+  });
+  return (
+    <div>
+      <h1>User Signup</h1>
+      <form onSubmit={formik.handleSubmit}>
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={formik.values.name}
+          onChange={formik.handleChange}
+        />
+        <br />
+        <br />
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={formik.values.email}
+          onChange={formik.handleChange}
+        />
+        <br />
+        <br />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={formik.values.password}
+          onChange={formik.handleChange}
+        />
+        <br />
+        <br />
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
+};
+
+export default Signup;
